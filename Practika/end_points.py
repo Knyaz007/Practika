88cab1ae92569d74167094ff3edd8748934ce779
Adminchik <Adminchik@LAPTOP-ONUG6SAQ>
2023-06-28 15:00:44 +0300

    pyproject.toml

--- Practika/end_points.py
+++ Practika/end_points.py
@@ -3,6 +3,23 @@ from typing import Dict
 
 app = FastAPI()
 
+# Функция для получения Swagger-схемы
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Your API Title",
+        version="1.0.0",
+        description="Your API description",
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+# Маршрут для получения Swagger-схемы
+@app.get("/openapi.json")
+async def get_openapi_endpoint():
+    return custom_openapi()
 
 # Пользователи (Users)
 
